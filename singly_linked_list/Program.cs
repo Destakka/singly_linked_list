04defa94180badbf2d5047a5845cfeac41241e07
DESTA ATIKANANDA <d.atikananda.ft21@mail.umy.ac.id>
2022-11-21 20:22:43 +0700

    add public void addNote

--- singly_linked_list/Program.cs
+++ singly_linked_list/Program.cs
@@ -20,5 +20,29 @@ namespace singly_linked_list
         {
             START = null;
         }
+        public void addNote()
+        {
+            int nim;
+            string nm;
+            Console.Write("\nEnter the roll number of the student: ");
+            nim = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the name of the student : ");
+            nm = Console.ReadLine();
+            node newnode = new node();
+            newnode.rollNumber = nim;
+            newnode.name = nm;
+            //if the node to be inserted is the first node 
+            if (START == null || nim <= START.rollNumber) ;
+            {
+                if ((START != null) && (nim == START.rollNumber)) ;
+                {
+                    Console.WriteLine("\nDuplicate roll numbers not allowed\n");
+                    return;
+                }
+                newnode.next = START;
+                START = newnode;
+                return;
+            }
+        }
     }
 }
