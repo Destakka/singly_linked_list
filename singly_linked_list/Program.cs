7948633647a0665ba47929f4e57d5a015e781ff4
DESTA ATIKANANDA <d.atikananda.ft21@mail.umy.ac.id>
2022-11-21 20:11:23 +0700

    add class node

--- singly_linked_list/Program.cs
+++ singly_linked_list/Program.cs
@@ -6,5 +6,11 @@ using System.Linq;
 namespace singly_linked_list
 
 {
-
+    //each node consist of the information part and like to the next node
+    class node
+    {
+        public int rollNumber;
+        public string name;
+        public node next;
+    }
 }
